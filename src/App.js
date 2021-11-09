e3bd4ecbe9f62cf5cfe4913004e1c336b2fc5042
Jahid <jahidulhaque767@gmail.com>
2021-11-10 04:49:39 +0600

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import AuthProvider from "./Contexts/AuthProvider/AuthProvider";
 import Home from "./Pages/Home/Home/Home";
 import MoreProducts from "./Pages/MoreProducts/MoreProducts";
 
@@ -18,19 +19,21 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/moreProducts">
-            <MoreProducts></MoreProducts>
-          </Route>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/moreProducts">
+              <MoreProducts></MoreProducts>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3EQtUDyzmpT_bE0WqKBNm6RL5r7wefgQ",
+  authDomain: "rolls-royce-b09f6.firebaseapp.com",
+  projectId: "rolls-royce-b09f6",
+  storageBucket: "rolls-royce-b09f6.appspot.com",
+  messagingSenderId: "990722499859",
+  appId: "1:990722499859:web:7009bad5115456d28b26e6",
+};
+
+export default firebaseConfig;
