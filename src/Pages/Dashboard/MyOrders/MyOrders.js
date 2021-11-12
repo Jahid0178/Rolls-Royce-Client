814cc6cd70bf9693f0e2368ede23d81964129b72
Jahid <jahidulhaque767@gmail.com>
2021-11-12 16:06:40 +0600

    added my orders

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,10 +1,58 @@
-import { Container } from "@mui/material";
-import React from "react";
+import { Container, TableContainer } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import useAuth from "../../../hooks/useAuth";
 
 const MyOrders = () => {
+  const { user } = useAuth();
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:4000/purchaseProduct?email=${user.email}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, []);
+  console.log(orders);
   return (
     <Container>
       <h2>My Orders</h2>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Order Name</TableCell>
+              <TableCell align="center">Your Email</TableCell>
+              <TableCell align="center">Status</TableCell>
+              <TableCell align="center">Fat&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <img
+                    src={row.img}
+                    style={{ width: "50px", marginRight: "10px" }}
+                    alt=""
+                  />
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.email} </TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
