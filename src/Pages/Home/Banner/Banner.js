6bbe7dcd0fde6a49edd5c7f29a49647ac92fdaa8
Jahid <jahidulhaque767@gmail.com>
2021-11-13 00:42:04 +0600

    complete banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,52 @@
-import { Container } from "@mui/material";
+import { Carousel } from "react-bootstrap";
 import React from "react";
 
 const Banner = () => {
   return (
-    <Container>
-      <h2>This is banner</h2>
-    </Container>
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/pnB9DGf/pexels-dom-j-303316.jpg"
+            alt="First slide"
+            style={{ height: "90vh" }}
+          />
+          <Carousel.Caption>
+            <h3>Rolls Royce Boat Tail</h3>
+            <p>World's Most Premium & Luxurious Cars</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/rMRyb5S/pexels-jae-park-3764984.jpg"
+            alt="Second slide"
+            style={{ height: "90vh" }}
+          />
+
+          <Carousel.Caption>
+            <h3>Rolls Royce Ghost</h3>
+            <p>World's Most Premium & Luxurious Cars</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ibb.co/Y2xCVyz/photo-1552561040-3ff08bed3eba.jpg"
+            alt="Third slide"
+            style={{ height: "90vh" }}
+          />
+
+          <Carousel.Caption>
+            <h3 style={{ color: "#000" }}>Rolls Royce Dawn</h3>
+            <p style={{ color: "#000" }}>
+              World's Most Premium & Luxurious Cars
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
   );
 };
 
